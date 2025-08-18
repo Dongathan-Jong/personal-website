91c8f1a7a5d289383a27d32da7e528b1be1cc598
Jonathan <jonathan.dong2@student.tdsb.on.ca>
2025-08-18 19:10:28 -0400

    rahhhhhhhhhh

--- personalwebsite/src/App.jsx
+++ personalwebsite/src/App.jsx
@@ -24,23 +24,49 @@ function App() {
         </div>
       </div>
 
+      
       <div className="bg-black w-full flex flex-col items-center justify-center">
-        <div className="bg-yellow-700 flex flex-col w-screen h-[1000px] items-center justify-center">
+        <div className="bg-black flex flex-col w-screen h-[1000px] items-center justify-center">
           <p className="biorhyme-expanded-extrabold text-[10vw] mt-4">Skills</p>
 
+          <p className="outfit mb-4">Hardware</p>
           <div className="grid grid-cols-5 gap-8 h-200">
-            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl">
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/arduino.png" alt="Arduino" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
             </div>
-            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl">
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/java.png" alt="Java" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
             </div>
-            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl">
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/html.png" alt="HTML" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
             </div>
-            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl">
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/kicad.png" alt="KiCad" className="w-full h-full object-contain rounded-2xl"/>
             </div>
-            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl">
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/f360.png" alt="Fusion 360" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
             </div>
-            
           </div>
+
+          <p className="outfit mb-4">Software</p>
+          <div className="grid grid-cols-5 gap-8 h-200">
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/arduino.png" alt="Arduino" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
+            </div>
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/java.png" alt="Java" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
+            </div>
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/html.png" alt="HTML" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
+            </div>
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/kicad.png" alt="KiCad" className="w-full h-full object-contain rounded-2xl"/>
+            </div>
+            <div className="bg-black w-[12vw] h-[12vw] rounded-3xl items-center justify-center flex skillshadow">
+              <img src="/assets/pictures/f360.png" alt="Fusion 360" className="w-4/5 h-4/5 object-contain rounded-2xl"/>
+            </div>
+          </div>
+
          
         </div>
       </div>
